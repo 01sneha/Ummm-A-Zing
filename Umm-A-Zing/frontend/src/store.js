0fcf4a7807003c802588771adf17165b7c638122
Sneha Mahajan <sneha01mahajan@gmail.com>
2021-04-10 10:04:12 +0530

    Implemented payment option using PayPal

--- Umm-A-Zing/frontend/src/store.js
+++ Umm-A-Zing/frontend/src/store.js
@@ -2,6 +2,11 @@ import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
 import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer,
+} from './reducers/orderReducers';
+import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers';
@@ -33,6 +38,9 @@ const reducer = combineReducers({
   cart: cartReducer,
   userSignin: userSigninReducer,
   userRegister: userRegisterReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
